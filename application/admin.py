a125e83f4c0a6c8e72e5d14f931cf576e279d211
eliaszpiotr <Eliasz.Piotr02@gmail.com>
2023-01-11 15:19:44 +0100

    static

--- application/admin.py
+++ application/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from models import Account
+from .models import Account
 
 
 class AccountAdmin(UserAdmin):
+    ordering = ('email',)
     list_display = ('email', 'first_name', 'last_name', 'date_joined', 'last_login', 'is_admin', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     readonly_fields = ('date_joined', 'last_login')
