28e889f9d2ae5f18d055b3ef3c8474dee3a96355
eliaszpiotr <Eliasz.Piotr02@gmail.com>
2023-01-11 15:17:59 +0100

    urls

--- application/urls.py
+++ application/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import LandingPage, AddDonation, Login, Register
 
 urlpatterns = [
+    path('', LandingPage.as_view(), name='landing_page'),
+    path('add_donation/', AddDonation.as_view(), name='add_donation'),
+    path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
     ]
